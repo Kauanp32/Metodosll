3fa6375ba145a609e6819ad2a7d8002b268a27a6
Kauan <kauan.p.clementino@gmail.com>
2023-11-20 21:38:30 -0300

    first commit

---
+++ Contagem.java
@@ -0,0 +1,31 @@
+package Metodosll;
+
+import java.util.Scanner;
+
+public class Contagem {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		for (int i = 0; i < 10; i++) {
+			System.out.print(i+1);
+		}
+		
+		System.out.print("Digite o numero onde a contagem sera encerrada: ");
+		 int fim = sc.nextInt();
+		 for (int i = 0; i < fim; i++) {
+			 System.out.println(i);
+			
+		 }
+		  System.out.println("Digite o numero onde a contagem comecara: ");
+		  int inicio1 = sc.nextInt();
+		  System.out.print("Digite o numero onde a contagem se encerrara: ");
+		  int fim2 = sc.nextInt();
+		  for (int i = inicio1; i <= fim2; i++) {
+			  System.out.print(i);
+		  }
+		  
+
+							
+		}
+
+}
