7a3400d0b0a1f692f91116ed91d3833d979ee778
Kauan <kauan.p.clementino@gmail.com>
2023-12-31 17:18:22 -0300

    first commit

--- Contagem.java
+++ Contagem.java
@@ -7,21 +7,21 @@ public class Contagem {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		for (int i = 0; i < 10; i++) {
-			System.out.print(i+1);
+			System.out.println(i+1);
 		}
 		
-		System.out.print("Digite o numero onde a contagem sera encerrada: ");
+		System.out.println("Digite o numero onde a contagem sera encerrada : ");
 		 int fim = sc.nextInt();
 		 for (int i = 0; i < fim; i++) {
 			 System.out.println(i);
 			
 		 }
-		  System.out.println("Digite o numero onde a contagem comecara: ");
+		  System.out.println("Digite o numero onde a contagem comecara : ");
 		  int inicio1 = sc.nextInt();
-		  System.out.print("Digite o numero onde a contagem se encerrara: ");
+		  System.out.println("Digite o numero onde a contagem se encerrara : ");
 		  int fim2 = sc.nextInt();
 		  for (int i = inicio1; i <= fim2; i++) {
-			  System.out.print(i);
+			  System.out.println(i);
 		  }
 		  
 
